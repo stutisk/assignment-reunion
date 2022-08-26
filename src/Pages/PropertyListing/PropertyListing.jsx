af0f7dab2e57c03cbf046ec846041f8d74f6467c
stutisk <48529714+stutisk@users.noreply.github.com>
2022-08-27 03:33:38 +0530

    feat:added searchbar

--- src/Pages/PropertyListing/PropertyListing.jsx
+++ src/Pages/PropertyListing/PropertyListing.jsx
@@ -3,6 +3,7 @@ import { Filter } from "../../Components/Filter/Filter";
 import { useFilter } from "../../Context/Filter/FilterContext";
 import { getFiltereddata } from "../../utils/getFiltereddata";
 import { useState } from "react";
+import styles from "../PropertyListing/PropertyListing.module.css"
 
 import data from "../../data.json";
 const PropertyListing = () => {
@@ -11,10 +12,14 @@ const PropertyListing = () => {
   const FinalProductList = getFiltereddata(state, property);
   return (
     <div className="pading-l-r ">
-      <h1>Search properties for rent</h1>
+      <div className="flex flex-space-between">
+        <h1>Search properties for rent</h1>
+       <div ><input type="text" placeholder="Search With SearchBar..." name="search" className={` ${styles.searchbar}`} /></div> 
+      </div>
+
       <Filter />
       <div className=" flex flex-wrap gap-cards ">
-        { FinalProductList?.map((property) => (
+        {FinalProductList?.map((property) => (
           <Card property={property} key={property.id} />
         ))}
       </div>
