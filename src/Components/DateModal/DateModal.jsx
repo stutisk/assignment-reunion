b84a3899de3c43c30c546f3ed8f791e1527769c1
stutisk <48529714+stutisk@users.noreply.github.com>
2022-08-27 16:09:34 +0530

    minor change

--- src/Components/DateModal/DateModal.jsx
+++ src/Components/DateModal/DateModal.jsx
@@ -1,84 +1,23 @@
-import { useFilter } from "../../Context/Filter/FilterContext";
+
 import styles from "../DateModal/DateModal.module.css";
 const DateModal = () => {
-  const{dispatch}=useFilter();
-  const DateHandler = (e) => {
-    
-    if (e.target.checked) {
-      dispatch({ type: e.target.name, payload: e.target.value });
-      console.log(e.target.value)
-    }
-  };
+
   return (
     <div>
       <div className={`${styles.DateModal} border-radius1 btn-padding  `}>
-        <form className="pricelist">
-          <div className=" margin-top">
-            {" "}
-            <label for="date">
-              <input
-                onChange={(e) => DateHandler(e)}
-                type="radio"
-                name="DATE"
-                value={"28-Aug-2022"}
-                className="pointer"
-              />
-              28-Aug-2022
-            </label>
-          </div>
-          <div className=" margin-top">
-            {" "}
-            <label for="price">
-              <input
-                onChange={(e) => DateHandler(e)}
-                type="radio"
-                name="DATE"
-                value={"29-Aug-2022"}
-                className="pointer"
-              />
-              29-Aug-2022
-            </label>
-          </div>
-          <div className=" margin-top">
-            {" "}
-            <label for="price">
-              <input
-                onChange={(e) => DateHandler(e)}
-                type="radio"
-                name="DATE"
-                value={"30-Aug-2022"}
-                className="pointer"
-              />
-              30-Aug-2022
-            </label>
-          </div>
-          <div className=" margin-top">
-            {" "}
-            <label for="price">
-              <input
-                onChange={(e) => DateHandler(e)}
-                type="radio"
-                name="DATE"
-                value={"31-Aug-2022"}
-                className="pointer"
-              />
-              31-Aug-2022
-            </label>
-          </div>
-          <div className=" margin-top">
-            {" "}
-            <label for="price">
-              <input
-                onChange={(e) => DateHandler(e)}
-                type="radio"
-                name="DATE"
-                value={"1-Sep-2022"}
-                className="pointer"
-              />
-              1-Sep-2022
-            </label>
-          </div>
-        </form>
+    
+      <form action="https://example.com">
+
+    
+    <input type="date" name="date"/>
+
+
+  <p> <button
+              className={`btn-padding pointer border-radius1 btn-primary font-bolder`}
+            >
+             Find
+            </button></p>
+</form>
       </div>
     </div>
   );
